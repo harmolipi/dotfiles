1373fde5e2b714a9a251e9d7d9ab2b7b4aef53fe
Niko <nikob381@gmail.com>
2022-07-22 11:16:17 -0400

    feat(nvim): add cmp mapping and config
    Add CMP plugin configuration from documentation

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -1,7 +1,13 @@
 -- CMP
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local luasnip = require("luasnip")
 
 vim.cmd([[set completeopt=menu,menuone,noselect]]) -- Should work from vimscript
 
@@ -20,6 +26,26 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' }, 
@@ -29,7 +55,9 @@ cmp.setup({
   }),
   formatting = {
     format = lspkind.cmp_format({
-      with_text = false,
+      mode = 'symbol_text',
+      preset = 'default',
+      -- with_text = false,
       maxwidth = 50,
     })
   }
