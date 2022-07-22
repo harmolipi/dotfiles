b0bea918d6dde7dae11b0a08cbc17b1aa01d5b30
Niko <nikob381@gmail.com>
2022-07-22 11:51:57 -0400

    refactor(nvim): format with lua-fmt

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -1,71 +1,85 @@
 -- CMP
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local cmp = require('cmp')
-local lspkind = require('lspkind')
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local luasnip = require("luasnip")
 
 vim.cmd([[set completeopt=menu,menuone,noselect]]) -- Should work from vimscript
 
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' }, 
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text',
-      preset = 'default',
-      -- with_text = false,
-      maxwidth = 50,
-    })
-  }
-})
-
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
+cmp.setup(
+    {
+        snippet = {
+            expand = function(args)
+                require("luasnip").lsp_expand(args.body)
+            end
+        },
+        mapping = cmp.mapping.preset.insert(
+            {
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.close(),
+                ["<CR>"] = cmp.mapping.confirm(
+                    {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true
+                    }
+                ),
+                ["<Tab>"] = cmp.mapping(
+                    function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end,
+                    {"i", "s"}
+                ),
+                ["<S-Tab>"] = cmp.mapping(
+                    function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end,
+                    {"i", "s"}
+                )
+            }
+        ),
+        sources = cmp.config.sources(
+            {
+                {name = "nvim_lsp"},
+                {name = "luasnip"}
+            },
+            {
+                {name = "buffer"}
+            }
+        ),
+        formatting = {
+            format = lspkind.cmp_format(
+                {
+                    mode = "symbol_text",
+                    preset = "default",
+                    -- with_text = false,
+                    maxwidth = 50
+                }
+            )
+        }
+    }
 )
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 vim.cmd([[highlight! default link CmpItemKind CmpItemMenuDefault]])
--- nvim/after/plugin/neoformat.rc.lua
+++ nvim/after/plugin/neoformat.rc.lua
@@ -1,13 +1,13 @@
 -- General settings
-vim.g.neoformat_basic_format_align = 1   -- Enable alignment
-vim.g.neoformat_basic_format_retab = 1   -- Enable tab to spaces conversion
-vim.g.neoformat_basic_format_trim = 1    -- Enable trimming of trailing whitespace
+vim.g.neoformat_basic_format_align = 1 -- Enable alignment
+vim.g.neoformat_basic_format_retab = 1 -- Enable tab to spaces conversion
+vim.g.neoformat_basic_format_trim = 1 -- Enable trimming of trailing whitespace
 
 -- Enable languages
-vim.g.neoformat_enabled_javascript = { "prettier" }
-vim.g.neoformat_enabled_html = { "prettier" }
-vim.g.neoformat_enabled_css = { "prettier" }
-vim.g.neoformat_enabled_php = { "prettier" }
-vim.g.neoformat_enabled_json = { "prettier" }
-vim.g.neoformat_enabled_yaml = { "prettier" }
+vim.g.neoformat_enabled_javascript = {"prettier"}
+vim.g.neoformat_enabled_html = {"prettier"}
+vim.g.neoformat_enabled_css = {"prettier"}
+vim.g.neoformat_enabled_php = {"prettier"}
+vim.g.neoformat_enabled_json = {"prettier"}
+vim.g.neoformat_enabled_yaml = {"prettier"}
 vim.g.neoformat_enabled_lua = {"luafmt"}
