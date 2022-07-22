e3685fa9006bc6dbd7623f40f7cd16f3d1018c7f
Niko <nikob381@gmail.com>
2022-07-22 13:38:39 -0400

    feat(nvim): configure to use local lua-fmt exe
    Configure neoformat to look for lua-fmt exe in local project directory

--- nvim/after/plugin/neoformat.rc.lua
+++ nvim/after/plugin/neoformat.rc.lua
@@ -2,6 +2,7 @@
 vim.g.neoformat_basic_format_align = 1 -- Enable alignment
 vim.g.neoformat_basic_format_retab = 1 -- Enable tab to spaces conversion
 vim.g.neoformat_basic_format_trim = 1 -- Enable trimming of trailing whitespace
+vim.g.neoformat_try_node_exe = 1 -- Look for exe in local directories
 
 -- Enable languages
 vim.g.neoformat_enabled_javascript = {"prettier"}
@@ -10,4 +11,9 @@ vim.g.neoformat_enabled_css = {"prettier"}
 vim.g.neoformat_enabled_php = {"prettier"}
 vim.g.neoformat_enabled_json = {"prettier"}
 vim.g.neoformat_enabled_yaml = {"prettier"}
+
+vim.g.neoformat_lua_luafmt = {
+    exe = "luafmt",
+    try_node_exe = 1 -- Check local directories for luafmt
+}
 vim.g.neoformat_enabled_lua = {"luafmt"}
