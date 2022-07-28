3489eb1d9f875706ed0cf7bfc51fb4e1f1a4a386
Niko <nikob381@gmail.com>
2022-07-28 00:23:03 -0400

    feat(nvim): reconfigure following chris@machine

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -1,15 +1,29 @@
+local cmp_status, cmp = pcall(require, "cmp")
+if not cmp_status then
+    return
+end
+
+local snip_status, luasnip = pcall(require, "luasnip")
+if not snip_status then
+    return
+end
+
 -- CMP
 local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local cmp = require("cmp")
 local lspkind = require("lspkind")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local luasnip = require("luasnip")
+require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.cmd([[set completeopt=menu,menuone,noselect]]) -- Should work from vimscript
+
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
 
 cmp.setup(
     {
@@ -20,24 +34,26 @@ cmp.setup(
         },
         mapping = cmp.mapping.preset.insert(
             {
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm(
-                    {
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true
-                    }
-                ),
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                ["<C-j>"] = cmp.mapping.select_next_item(),
+                ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), {"i", "c"}),
+                ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), {"i", "c"}),
+                ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+                ["<C-e>"] = cmp.mapping {
+                    i = cmp.mapping.abort(),
+                    c = cmp.mapping.close()
+                },
+                ["<CR>"] = cmp.mapping.confirm({select = true}),
                 ["<Tab>"] = cmp.mapping(
                     function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
+                        elseif luasnip.expandable() then
+                            luasnip.expand()
                         elseif luasnip.expand_or_jumpable() then
                             luasnip.expand_or_jump()
                         elseif has_words_before() then
-                            cmp.complete()
+                            fallback()
                         else
                             fallback()
                         end
@@ -61,21 +77,43 @@ cmp.setup(
         sources = cmp.config.sources(
             {
                 {name = "nvim_lsp"},
-                {name = "luasnip"}
-            },
-            {
-                {name = "buffer"}
+                {name = "nvim_lua"},
+                {name = "luasnip"},
+                {name = "buffer"},
+                {name = "path"}
             }
         ),
         formatting = {
             format = lspkind.cmp_format(
                 {
                     mode = "symbol_text",
+                    menu = ({
+                        buffer = "[Buffer]",
+                        nvim_lsp = "[LSP]",
+                        luasnip = "[LuaSnip]",
+                        nvim_lua = "[Lua]"
+                    }),
                     preset = "default",
-                    -- with_text = false,
                     maxwidth = 50
                 }
             )
+        },
+        confirm_opts = {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = false
+        },
+        window = {
+            documentation = {
+                border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"},
+                winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None"
+            },
+            completion = {
+                border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"},
+                winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None"
+            }
+        },
+        experimental = {
+            ghost_text = true
         }
     }
 )
