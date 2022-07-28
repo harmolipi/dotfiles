b95d78dd8d376b8bd4a7651e0f767a35a161f367
Niko <nikob381@gmail.com>
2022-07-28 00:26:56 -0400

    refactor(nvim): refactor treesitter config

--- nvim/after/plugin/nvim-treesitter.rc.lua
+++ nvim/after/plugin/nvim-treesitter.rc.lua
@@ -1,5 +1,9 @@
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
 -- Nvim-treesitter
-require("nvim-treesitter.configs").setup {
+treesitter.setup {
     auto_install = true,
     highlight = {
         enable = true
