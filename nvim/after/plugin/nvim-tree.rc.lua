462d560445426196754c22bb45b0e9dfa96f0ebb
Niko <nikob381@gmail.com>
2022-07-30 10:04:07 -0400

    feat(nvim): configure based on chris@machine video

--- nvim/after/plugin/nvim-tree.rc.lua
+++ nvim/after/plugin/nvim-tree.rc.lua
@@ -1,9 +1,35 @@
 -- Nvim-tree
-require("nvim-tree").setup {
+
+local nvim_tree_status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not nvim_tree_status_ok then
+  return
+end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
+nvim_tree.setup {
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
     view = {
+        side = "left",
         mappings = {
             list = {
-                {key = "so", action = "system_open"} -- Remove 's' key conflict with moving windows shortcut
+                {key = "so", action = "system_open"}, -- Remove 's' key conflict with moving windows shortcut
+                {key = "l", cb = tree_cb "edit"},
+                {key = "h", cb = tree_cb "close_node"},
             }
         }
     },
