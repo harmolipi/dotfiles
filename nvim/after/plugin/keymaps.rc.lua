93dc651bcbc2786f4c65fb4adb1d542018ced3eb
Niko <nikob381@gmail.com>
2022-08-01 10:33:17 -0400

    feat(nvim): add useful keymaps

--- nvim/after/plugin/keymaps.rc.lua
+++ nvim/after/plugin/keymaps.rc.lua
@@ -10,3 +10,20 @@ keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
 -- Null-ls
 keymap("n", "<leader>fo", ":Format<CR>", opts)
+
+-- Visual
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>=", opts)
+keymap("v", "<A-k>", ":m .-2<CR>=", opts)
+
+-- Hold copy in clipboard
+keymap("v", "p", '"_dp', opts)
+
+-- Visual Block
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
