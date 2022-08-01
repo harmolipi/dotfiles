b4127a8461a58344cfbb32f66f11cd0d6e695fbc
Niko <nikob381@gmail.com>
2022-08-01 10:30:39 -0400

    refactor(nvim): use require variable

--- nvim/after/plugin/lsp-handlers.lua
+++ nvim/after/plugin/lsp-handlers.lua
@@ -5,7 +5,7 @@ if not status_cmp then
 	return
 end
 
-M.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+M.capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 M.setup = function()
