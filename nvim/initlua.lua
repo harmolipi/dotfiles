1573469c08f1f26855e4a0014917819fd3c4b532
Niko <nikob381@gmail.com>
2022-08-01 10:37:24 -0400

    refactor(nvim): start moving some settings to lua init file

---
+++ nvim/initlua.lua
@@ -0,0 +1,5 @@
+local options = vim.opt
+
+options.backupskip =
+	"set backupskip=$TMPDIR/*,$TMP/*,$TEMP/*,/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,/private/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*"
+options.showcmd = false
