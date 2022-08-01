b92d8f6e1cebee5ff4d8a571b5049743ef3a06a3
Niko <nikob381@gmail.com>
2022-08-01 10:34:36 -0400

    style(nvim): style with `null_ls`

--- nvim/after/plugin/nvim-lsp-installer.rc.lua
+++ nvim/after/plugin/nvim-lsp-installer.rc.lua
@@ -1,7 +1,7 @@
 -- Nvim-lsp-installer
 local status, lsp_installer = pcall(require, "nvim-lsp-installer")
-if (not status) then
-    return
+if not status then
+	return
 end
 
 package.path = "/Users/nikobirbilis/.config/nvim/after/plugin/?.lua;" .. package.path
@@ -9,59 +9,65 @@ local handlers = require("lsp-handlers")
 handlers.setup()
 
 local servers = {
-    "cssls",
-    "html",
-    "tsserver",
-    "jsonls",
-    "yamlls",
-    "eslint",
-    "intelephense",
-    "emmet_ls",
-    "cssmodules_ls",
-    "quick_lint_js",
-    "sumneko_lua",
-    "hoon_ls"
+	"cssls",
+	"html",
+	"tsserver",
+	"jsonls",
+	"yamlls",
+	"eslint",
+	"intelephense",
+	"emmet_ls",
+	"cssmodules_ls",
+	"quick_lint_js",
+	"sumneko_lua",
+	"hoon_ls",
 }
 
-lsp_installer.setup {
-    automatic_installation = true
-}
+lsp_installer.setup({
+	automatic_installation = true,
+})
 
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-    return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-    opts = {
-        on_attach = handlers.on_attach,
-        capabilities = handlers.capabilities
-    }
+	opts = {
+		on_attach = handlers.on_attach,
+		capabilities = handlers.capabilities,
+	}
 
-    if server == "sumneko_lua" then
-        local sumneko_opts = {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = {"vim"}
-                    },
-                    workspace = {
-                        library = {
-                            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                            [vim.fn.stdpath "config" .. "/lua"] = true
-                        }
-                    }
-                }
-            }
-        }
-        opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-    end
+	if server == "sumneko_lua" then
+		local sumneko_opts = {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		}
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
 
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+})
+
 -- Lspsaga
-local saga = require "lspsaga"
+local saga = require("lspsaga")
 saga.init_lsp_saga()
