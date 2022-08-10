87ace772f48b599df3d6376a0286a355fc78d1e0
Niko <nikob381@gmail.com>
2022-08-10 14:53:06 -0400

    refactor(nvim): consolidated keymaps

--- nvim/after/plugin/keymaps.rc.lua
+++ nvim/after/plugin/keymaps.rc.lua
@@ -1,15 +1,57 @@
--- Shorten function name
-local keymap = vim.keymap.set
--- Silent keymap option
-local opts = { silent = true }
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, silent = true, expr = true }
+
+-- in visual line mode do not join pasted text to next line
+keymap("v", "p", [[mode() ==# "V" && match(getreg(), "\n$") == -1 ? "\"_dd<esc>O<esc>p" : "\"_dP"]], expr_opts)
+-- paste text on new line, if there is already linebreak do not insert a new one
+keymap("n", "<leader>p", [[match(getreg(), "\n$") == -1 ? "o<esc>p" : "p"]], expr_opts)
+keymap("n", "<leader>P", [[match(getreg(), "\n$") == -1 ? "O<esc>p" : "P"]], expr_opts)
+
+-- move at the start and end of line easily
+keymap("", "H", "^", {})
+keymap("", "L", "$", {})
+
+-- buffers shortcuts
+keymap("n", "<leader>l", ":bnext<CR>", opts)
+keymap("n", "<leader>h", ":bprevious<CR>", opts)
+keymap("n", "<leader>d", ":bdelete<CR>", opts)
+keymap("n", "<leader>w", ":write<CR>", opts)
+keymap("n", "<leader>q", ":quit<CR>", opts)
+
+--  easier split window
+keymap("n", "<leader>|", ":vsplit<CR>", opts)
+keymap("n", "<leader>-", ":split<CR>", opts)
+
+-- edit common files quickly
+keymap("n", "<leader>ev", ":edit $MYVIMRC<cr>", opts)
+-- keymap("n", "<leader>ez", ":edit ~/.zshrc<cr>", opts)
+-- TODO: make sure this reloads lua modules
+keymap("n", "<leader>so", ":so $MYVIMRC <cr> | :echom 'Loaded' <cr>", opts)
+
+-- keep jumps and search in middle
+keymap("n", "n", "nzz", opts)
+keymap("n", "N", "Nzz", opts)
+keymap("n", "<C-o>", "<C-o>zz", opts)
+keymap("n", "<C-i>", "<C-i>zz", opts)
+
+-- copy whole buffer
+keymap("n", "<F5>", ":%y+<CR>", opts)
+
+-- escape: also clears highlighting
+keymap("n", "<esc>", "<Cmd>noh<return><esc>", opts)
+
+-- indent and keep stay in visualMode
+keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts)
 
 -- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
--- Null-ls
-keymap("n", "<leader>fo", ":Format<CR>", opts)
+-- Change color
+keymap("n", "<C-c>", [[:call CocAction('pickColor')<CR>]], opts)
 
 -- Visual
 -- Stay in indent mode
@@ -27,3 +69,4 @@ keymap("v", "p", '"_dp', opts)
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+
