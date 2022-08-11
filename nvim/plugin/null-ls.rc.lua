427e572a5297f2eb5ebc2e863a90290b6532fafb
Niko <nikob381@gmail.com>
2022-08-11 17:07:54 -0400

    feat(nvim): format on save

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -13,6 +13,7 @@ null_ls.setup {
           vim.lsp.buf.formatting_sync()
         end,
       })
+      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
     end
   end,
   sources = {
