b22adb6de4d5d54a172ea777f13ce4acae9b4276
Niko <nikob381@gmail.com>
2022-08-11 17:07:38 -0400

    refactor(nvim): use lua instead of vimscript

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -3,11 +3,16 @@ if (not status) then return end
 
 null_ls.setup {
   on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_command [[augroup Format]]
-      vim.api.nvim_command [[autocmd! * <buffer>]]
-      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-      vim.api.nvim_command [[augroup END]]
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
     end
   end,
   sources = {
