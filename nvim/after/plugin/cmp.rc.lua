3fdb25c6db24da0c4572be0da94c56bfdc687f2f
Niko <nikob381@gmail.com>
2022-08-12 11:54:33 -0400

    fix(nvim): reorder completion suggestions

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -51,8 +51,8 @@ cmp.setup({
     end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'nvim_lsp' },
     { name = 'buffer' },
   }),
   formatting = {
