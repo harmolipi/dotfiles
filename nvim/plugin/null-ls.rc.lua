be2088cbe4a932f8796073f1e7a7c6fca04ee1ea
Niko <nikob381@gmail.com>
2022-08-12 12:05:18 -0400

    refactor(nvim): remove redundant format-on-save call

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -9,11 +9,9 @@ null_ls.setup {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
           vim.lsp.buf.formatting_sync()
         end,
       })
-      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
     end
   end,
   sources = {
