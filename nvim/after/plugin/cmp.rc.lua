3ea5811f84aeab7fd22be954e3d8276d142fc73d
Niko <nikob381@gmail.com>
2022-09-24 22:11:53 -0400

    fix(nvim): move luasnip down in completion list

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -51,8 +51,8 @@ cmp.setup({
     end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = 'luasnip' },
     { name = 'nvim_lsp' },
+    { name = 'luasnip' },
     { name = 'buffer' },
   }),
   formatting = {
