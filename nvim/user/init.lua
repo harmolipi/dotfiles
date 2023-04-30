420958541d6be2b1433b4cdcee11828d2946c6c7
Niko Birbilis <nikob381@gmail.com>
2023-04-30 14:06:35 -0400

    feat(tsserver): add default typechecking for js to tsserver

--- nvim/user/init.lua
+++ nvim/user/init.lua
@@ -42,6 +42,13 @@ return {
       timeout_ms = 1000, -- default format timeout
     },
     config = {
+      tsserver = {
+        settings = {
+          implicitProjectConfiguration = {
+            checkJs = true,
+          }
+        }
+      },
       intelephense = {
         settings = {
           intelephense = {
