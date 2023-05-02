7f148bcb6fbb52f296e2f080f3cb538a38db1d02
Niko Birbilis <nikob381@gmail.com>
2023-05-01 23:23:52 -0400

    feat(dap): set up firefox js dap

--- nvim/user/plugins/mason.lua
+++ nvim/user/plugins/mason.lua
@@ -20,7 +20,29 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      handlers = {
+        function(config)
+          -- all sources with no handler get passed here
+
+          -- Keep original functionality
+          require('mason-nvim-dap').default_setup(config)
+        end,
+        firefox = function(config)
+          local dap = require "dap"
+
+          dap.configurations.javascript = {
+            {
+              name = 'Debug with Firefox',
+              type = 'firefox',
+              request = 'launch',
+              reAttach = true,
+              url = 'http://localhost:8080',
+              webRoot = '${workspaceFolder}',
+            }
+          }
+          require('mason-nvim-dap').default_setup(config) -- don't forget this!
+        end,
+      }
     },
   },
 }
