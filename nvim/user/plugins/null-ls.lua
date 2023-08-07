569892be6af95a7cfc9521a4e08f7e021960f40d
Niko Birbilis <nikob381@gmail.com>
2023-08-07 00:16:08 -0400

    style(null-ls): autoformat

--- nvim/user/plugins/null-ls.lua
+++ nvim/user/plugins/null-ls.lua
@@ -1,20 +1,20 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
+	"jose-elias-alvarez/null-ls.nvim",
+	opts = function(_, config)
+		-- config variable is the default configuration table for the setup function call
+		local null_ls = require("null-ls")
 
-    -- Check supported formatters and linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- null_ls.builtins.diagnostics.eslint,
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
-      -- null_ls.builtins.diagnostics.stylelint,
-      -- null_ls.builtins.code_actions.eslint_d
-    }
-    return config -- return final config table
-  end,
+		-- Check supported formatters and linters
+		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+		config.sources = {
+			-- null_ls.builtins.diagnostics.eslint,
+			-- Set a formatter
+			-- null_ls.builtins.formatting.stylua,
+			-- null_ls.builtins.formatting.prettier,
+			-- null_ls.builtins.diagnostics.stylelint,
+			-- null_ls.builtins.code_actions.eslint_d
+		}
+		return config -- return final config table
+	end,
 }
