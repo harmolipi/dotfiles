d39b36cd8c217293dcc2da58f1e0edeb12f58c2a
Niko Birbilis <nikob381@gmail.com>
2023-12-19 11:10:52 -0500

    feat(neovim): add some new plugins

--- nvim/user/plugins/personal.lua
+++ nvim/user/plugins/personal.lua
@@ -148,7 +148,64 @@ return {
 		lazy = false,
 	},
 	{
-		"tpope/vim-fugitive",
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter",
+		},
+		config = function()
+			require("refactoring").setup()
+			vim.keymap.set("x", "<leader>re", ":Refactor extract ")
+			vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+
+			vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+
+			vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+
+			vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
+
+			vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
+			vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+			vim.keymap.set({ "n", "x" }, "<leader>rr", function()
+				require("telescope").extensions.refactoring.refactors()
+			end)
+		end,
 		event = "User AstroFile",
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		requires = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				max_lines = 2,
+				line_numbers = false,
+			})
+		end,
+		event = "User AstroFile",
+	},
+	{
+		"antosha417/nvim-lsp-file-operations",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-neo-tree/neo-tree.nvim",
+		},
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
+		event = "User AstroFile",
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup()
+		end,
+		event = "UIEnter",
+	},
+	-- {
+	-- 	"nvim-neo-tree/neo-tree.nvim",
+	-- 	enabled = false,
+	-- },
 }
