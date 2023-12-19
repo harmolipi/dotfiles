120b9b9f83b053798d7e8c62080bd2a4620a6b27
Niko Birbilis <nikob381@gmail.com>
2023-12-19 10:16:38 -0500

    neovim - remove unused keybindings

--- nvim/user/mappings.lua
+++ nvim/user/mappings.lua
@@ -10,12 +10,10 @@ return {
 			desc = "Pick to close",
 		},
 		["<leader>b"] = { name = "Buffers" },
-		["<leader>pm"] = { ":PhpactorContextMenu<cr>" },
 		-- Devaslife mappings:
 		["x"] = { '"_x', desc = "Don't yank with 'x'" },
 		["+"] = { "<C-a>", desc = "Increment" },
 		["-"] = { "<C-x>", desc = "Decrement" },
-		["dw"] = { 'vb"_d', desc = "Delete a word backwards" },
 		["<C-a>"] = { "gg<S-v>G", desc = "Select all" },
 		-- Primeagen mappings:
 		["<leader>d"] = { '"_d', desc = "Don't yank with 'd'" },
