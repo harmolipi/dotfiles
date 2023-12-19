b99951589efa47103e1225f679b8596771da8444
Niko Birbilis <nikob381@gmail.com>
2023-12-19 10:06:22 -0500

    fix(neovim): add fix for luasnip mappings and autoformat

--- nvim/user/init.lua
+++ nvim/user/init.lua
@@ -1,161 +1,188 @@
 return {
-  -- Configure AstroNvim updates
-  updater = {
-    remote = "origin",     -- remote to use
-    channel = "stable",    -- "stable" or "nightly"
-    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly",    -- branch name (NIGHTLY ONLY)
-    commit = nil,          -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false,  -- skip prompts about breaking changes
-    show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false,     -- automatically quit the current session after a successful update
-    remotes = {            -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-      --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-      --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    },
-  },
-  -- Set colorscheme to use
-  colorscheme = "onedark",
-  -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
-  },
-  lsp = {
-    -- customize lsp formatting options
-    formatting = {
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
-      },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- "sumneko_lua",
-      },
-      timeout_ms = 1000, -- default format timeout
-    },
-    config = {
-      -- tsserver = {
-      --   settings = {
-      --     implicitProjectConfiguration = {
-      --       checkJs = true,
-      --     }
-      --   }
-      -- },
-      intelephense = {
-        settings = {
-          intelephense = {
-            stubs = {
-              "apache",
-              "bcmath",
-              "bz2",
-              "calendar",
-              "com_dotnet",
-              "Core",
-              "ctype",
-              "curl",
-              "date",
-              "dba",
-              "dom",
-              "enchant",
-              "exif",
-              "FFI",
-              "fileinfo",
-              "filter",
-              "fpm",
-              "ftp",
-              "gd",
-              "gettext",
-              "gmp",
-              "hash",
-              "iconv",
-              "imap",
-              "intl",
-              "json",
-              "ldap",
-              "libxml",
-              "mbstring",
-              "meta",
-              "mysqli",
-              "oci8",
-              "odbc",
-              "openssl",
-              "pcntl",
-              "pcre",
-              "PDO",
-              "pdo_ibm",
-              "pdo_mysql",
-              "pdo_pgsql",
-              "pdo_sqlite",
-              "pgsql",
-              "Phar",
-              "posix",
-              "pspell",
-              "readline",
-              "Reflection",
-              "session",
-              "shmop",
-              "SimpleXML",
-              "snmp",
-              "soap",
-              "sockets",
-              "sodium",
-              "SPL",
-              "sqlite3",
-              "standard",
-              "superglobals",
-              "sysvmsg",
-              "sysvsem",
-              "sysvshm",
-              "tidy",
-              "tokenizer",
-              "xml",
-              "xmlreader",
-              "xmlrpc",
-              "xmlwriter",
-              "xsl",
-              "Zend OPcache",
-              "zip",
-              "zlib",
-              "wordpress",
-              "phpunit",
-            },
-          },
-        },
-      },
-    },
-    -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
-    },
-  },
-  -- Configure require("lazy").setup() options
-  lazy = {
-    defaults = { lazy = true },
-    performance = {
-      rtp = {
-        -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin", "matchparen" },
-      },
-    },
-  },
-  -- This function is run last and is a good place to configuring
-  -- augroups/autocommands and custom filetypes also this just pure lua so
-  -- anything that doesn't fit in the normal config locations above can go here
-  polish = function()
-    local copilot_options = { silent = true, expr = true, script = true }
-    vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept(<Tab>)", copilot_options)
-  end,
-  heirline = {
-    separators = {
-      tab = { "", "" },
-    },
-  }
+	-- Configure AstroNvim updates
+	updater = {
+		remote = "origin",   -- remote to use
+		channel = "stable",  -- "stable" or "nightly"
+		version = "latest",  -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+		branch = "nightly",  -- branch name (NIGHTLY ONLY)
+		commit = nil,        -- commit hash (NIGHTLY ONLY)
+		pin_plugins = nil,   -- nil, true, false (nil will pin plugins on stable only)
+		skip_prompts = false, -- skip prompts about breaking changes
+		show_changelog = true, -- show the changelog after performing an update
+		auto_quit = false,   -- automatically quit the current session after a successful update
+		remotes = {          -- easily add new remotes to track
+			--   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+			--   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
+			--   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
+		},
+	},
+	-- Set colorscheme to use
+	colorscheme = "onedark",
+	-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+	diagnostics = {
+		virtual_text = true,
+		underline = true,
+	},
+	lsp = {
+		-- customize lsp formatting options
+		formatting = {
+			-- control auto formatting on save
+			format_on_save = {
+				enabled = true, -- enable or disable format on save globally
+				allow_filetypes = { -- enable format on save for specified filetypes only
+					-- "go",
+				},
+				ignore_filetypes = { -- disable format on save for specified filetypes
+					-- "python",
+				},
+			},
+			disabled = { -- disable formatting capabilities for the listed language servers
+				-- "sumneko_lua",
+			},
+			timeout_ms = 1000, -- default format timeout
+		},
+		config = {
+			-- tsserver = {
+			--   settings = {
+			--     implicitProjectConfiguration = {
+			--       checkJs = true,
+			--     }
+			--   }
+			-- },
+			intelephense = {
+				settings = {
+					intelephense = {
+						stubs = {
+							"apache",
+							"bcmath",
+							"bz2",
+							"calendar",
+							"com_dotnet",
+							"Core",
+							"ctype",
+							"curl",
+							"date",
+							"dba",
+							"dom",
+							"enchant",
+							"exif",
+							"FFI",
+							"fileinfo",
+							"filter",
+							"fpm",
+							"ftp",
+							"gd",
+							"gettext",
+							"gmp",
+							"hash",
+							"iconv",
+							"imap",
+							"intl",
+							"json",
+							"ldap",
+							"libxml",
+							"mbstring",
+							"meta",
+							"mysqli",
+							"oci8",
+							"odbc",
+							"openssl",
+							"pcntl",
+							"pcre",
+							"PDO",
+							"pdo_ibm",
+							"pdo_mysql",
+							"pdo_pgsql",
+							"pdo_sqlite",
+							"pgsql",
+							"Phar",
+							"posix",
+							"pspell",
+							"readline",
+							"Reflection",
+							"session",
+							"shmop",
+							"SimpleXML",
+							"snmp",
+							"soap",
+							"sockets",
+							"sodium",
+							"SPL",
+							"sqlite3",
+							"standard",
+							"superglobals",
+							"sysvmsg",
+							"sysvsem",
+							"sysvshm",
+							"tidy",
+							"tokenizer",
+							"xml",
+							"xmlreader",
+							"xmlrpc",
+							"xmlwriter",
+							"xsl",
+							"Zend OPcache",
+							"zip",
+							"zlib",
+							"wordpress",
+							"phpunit",
+						},
+					},
+				},
+			},
+		},
+		-- enable servers that you already have installed without mason
+		servers = {
+			-- "pyright"
+		},
+	},
+	-- Configure require("lazy").setup() options
+	lazy = {
+		defaults = { lazy = true },
+		performance = {
+			rtp = {
+				-- customize default disabled vim plugins
+				disabled_plugins = {
+					"tohtml",
+					"gzip",
+					"matchit",
+					"zipPlugin",
+					"netrwPlugin",
+					"tarPlugin",
+					"matchparen",
+				},
+			},
+		},
+	},
+	-- This function is run last and is a good place to configuring
+	-- augroups/autocommands and custom filetypes also this just pure lua so
+	-- anything that doesn't fit in the normal config locations above can go here
+	polish = function()
+		local copilot_options = { silent = true, expr = true, script = true }
+		vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept(<Tab>)", copilot_options)
+
+		-- stop snippets when you leave to normal mode
+		-- https://github.com/L3MON4D3/LuaSnip/issues/258
+		if require("astronvim.utils").is_available("luasnip") then
+			vim.api.nvim_create_autocmd("ModeChanged", {
+				pattern = "*",
+				group = vim.api.nvim_create_augroup("ModeChangedGroup", { clear = true }),
+				callback = function()
+					local luasnip = require("luasnip")
+					if
+							((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+							and luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+							and not luasnip.session.jump_active
+					then
+						luasnip.unlink_current()
+					end
+				end,
+			})
+		end
+	end,
+	heirline = {
+		separators = {
+			tab = { "", "" },
+		},
+	},
 }
