db69d9db2e088c8cffe35d22c9015f3fa8f31593
Niko Birbilis <nikob381@gmail.com>
2023-12-19 10:17:33 -0500

    neovim - start working on mappings for git fugitive

--- nvim/user/mappings.lua
+++ nvim/user/mappings.lua
@@ -1,4 +1,4 @@
-return {
+local custom_mappings = {
 	n = {
 		["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
 		["<leader>bD"] = {
@@ -22,6 +22,7 @@ return {
 			desc = "Find and replace current word",
 		},
 		["<leader>U"] = { vim.cmd.UndotreeToggle, desc = "Toggle UndoTree" },
+		-- ["<leader>gs"] = { ":tab Git<cr>", desc = "Open Fugitive status" },
 	},
 	t = {},
 	v = {
@@ -29,3 +30,51 @@ return {
 		["K"] = { ":m '<-2<cr>gv=gv", desc = "Move line up" },
 	},
 }
+
+-- local fugitive_group = vim.api.nvim_create_augroup("fugitive_group", {})
+--
+-- local autocmd = vim.api.nvim_create_autocmd
+-- autocmd("BufWinEnter", {
+-- 	group = fugitive_group,
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		if vim.bo.ft ~= "fugitive" then
+-- 			return
+-- 		end
+--
+-- 		local bufnr = vim.api.nvim_get_current_buf()
+--
+-- 		vim.keymap.set("n", "<Leader>gp", ":Git push<CR>", {
+-- 			desc = "Git push",
+-- 			buffer = bufnr,
+-- 			remap = false,
+-- 		})
+--
+-- 		vim.keymap.set("n", "<Leader>gP", ":Git pull --rebase<CR>", {
+-- 			desc = "Git pull with rebase",
+-- 			buffer = bufnr,
+-- 			remap = false,
+-- 		})
+--
+-- 		vim.keymap.set("n", "q", "<space>c", {
+-- 			desc = "Exit Fugitive",
+-- 			buffer = bufnr,
+-- 			remap = true,
+-- 			silent = true,
+-- 		})
+--
+-- 		vim.keymap.set("n", "?", "g?", {
+-- 			desc = "Open Fugitive help",
+-- 			buffer = bufnr,
+-- 			remap = true,
+-- 		})
+--
+-- 		vim.keymap.set("n", "<space>", "-", {
+-- 			desc = "Stage file under cursor",
+-- 			buffer = bufnr,
+-- 			remap = true,
+-- 		})
+-- 	end,
+-- })
+
+return custom_mappings
