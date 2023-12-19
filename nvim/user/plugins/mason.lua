054cc7c86e643c6d7d7774e3b7ef889daed44b03
Niko Birbilis <nikob381@gmail.com>
2023-12-19 10:04:08 -0500

    feat(mason): add formatters and debuggers

--- nvim/user/plugins/mason.lua
+++ nvim/user/plugins/mason.lua
@@ -1,48 +1,56 @@
 -- customize mason plugins
 return {
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = {
-      -- ensure_installed = { "lua_ls" },
-    },
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = {
-      -- ensure_installed = { "prettier", "stylua" },
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      handlers = {
-        function(config)
-          -- all sources with no handler get passed here
+	-- use mason-lspconfig to configure LSP installations
+	{
+		"williamboman/mason-lspconfig.nvim",
+		-- overrides `require("mason-lspconfig").setup(...)`
+		opts = {
+			ensure_installed = {
+				"jsonls",
+				"intelephense",
+				"tailwindcss",
+				"emmet_ls",
+				"html",
+				"tsserver",
+			},
+		},
+	},
+	-- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+	{
+		"jay-babu/mason-null-ls.nvim",
+		-- overrides `require("mason-null-ls").setup(...)`
+		opts = {
+			ensure_installed = { "prettier", "black", "gofmt" },
+		},
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		-- overrides `require("mason-nvim-dap").setup(...)`
+		opts = {
+			ensure_installed = { "firefox", "delve" },
+			handlers = {
+				function(config)
+					-- all sources with no handler get passed here
 
-          -- Keep original functionality
-          require('mason-nvim-dap').default_setup(config)
-        end,
-        firefox = function(config)
-          local dap = require "dap"
+					-- Keep original functionality
+					require("mason-nvim-dap").default_setup(config)
+				end,
+				firefox = function(config)
+					local dap = require("dap")
 
-          dap.configurations.javascript = {
-            {
-              name = 'Debug with Firefox',
-              type = 'firefox',
-              request = 'launch',
-              reAttach = true,
-              url = 'http://localhost:8080',
-              webRoot = '${workspaceFolder}',
-            }
-          }
-          require('mason-nvim-dap').default_setup(config) -- don't forget this!
-        end,
-      }
-    },
-  },
+					dap.configurations.javascript = {
+						{
+							name = "Debug with Firefox",
+							type = "firefox",
+							request = "launch",
+							reAttach = true,
+							url = "http://localhost:8080",
+							webRoot = "${workspaceFolder}",
+						},
+					}
+					require("mason-nvim-dap").default_setup(config) -- don't forget this!
+				end,
+			},
+		},
+	},
 }
