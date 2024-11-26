a904fdf8d9a0faf70cb5da93d6a42aec44005a6d
Niko Birbilis <nikob381@gmail.com>
2024-11-25 22:50:37 -0500

    rely on manual nvim dotfile management for now

--- nixos-config/home.nix
+++ nixos-config/home.nix
@@ -9,16 +9,16 @@
     pkgs.nixpkgs-fmt
   ];
 
-  programs.neovim = {
-    enable = true;
-    extraConfig = "./nvim/init.lua";
-  };
+  # programs.neovim = {
+  #   enable = true;
+  #   extraConfig = "./nvim/init.lua";
+  # };
 
   #home.file = {
-    #".config/nvim" = {
-      # source = toString (outPath + "/nvim");
-      #source = ./nvim/lua;
-      #recursive = true;
-    #};
+  #".config/nvim" = {
+  # source = toString (outPath + "/nvim");
+  #source = ./nvim/lua;
+  #recursive = true;
+  #};
   #};
 }
