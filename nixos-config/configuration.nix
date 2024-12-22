c1f74d872500a57d60bca0e49a8dda15e96c9d33
Niko Birbilis <nikob381@gmail.com>
2024-12-21 23:25:58 -0500

    clean up config
    graphics settings seemed to already be a couple lines up

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -89,7 +89,6 @@ in
     extraPackages = with pkgs; [ libva vaapiVdpau libvdpau-va-gl libgpg-error ];
   };
 
-  hardware.opengl.extraPackages = with pkgs; [ libva vaapiVdpau libvdpau-va-gl ];
   hardware.uinput.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -284,7 +283,6 @@ in
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  # system.stateVersion = "24.05"; # Did you read the comment?
   system.stateVersion = "24.05"; # Did you read the comment?
 
   virtualisation.docker.enable = true;
