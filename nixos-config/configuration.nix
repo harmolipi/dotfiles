4f9a02d4de0a27a8652fcff690b063d20a92c32f
Niko Birbilis <nikob381@gmail.com>
2025-11-13 10:10:02 -0500

    remove outdated fingerprint reader config

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -204,11 +204,6 @@ in
 
   services.fprintd = {
     enable = true;
-    package = pkgs.fprintd-tod;
-    tod = {
-      enable = true;
-      driver = pkgs.libfprint-2-tod1-vfs0090;
-    };
   };
 
   # Install firefox.
